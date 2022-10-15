78475bd6b6bd708465e64aafbe033f9686baf6f8
binnev <byntje@gmail.com>
2022-10-15 15:28:59 +0200

    commented out recording stuff

--- game.py
+++ game.py
@@ -18,8 +18,8 @@ class DinoJump(Game):
     window_height = conf.WINDOW_HEIGHT
     window_caption = "Dino Jump"
     screen_color = (130, 130, 130)
-    record_last_n_frames = 60 * 60 * 2
-    recording_dir = Path(__file__).parent / "recordings"
+    # record_last_n_frames = 60 * 60 * 2
+    # recording_dir = Path(__file__).parent / "recordings"
 
     def __init__(self):
         super().__init__()
